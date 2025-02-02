4a92ea0841725b6af45c8f633ea112f8278e741a
cswzchenyang <2159822551@qq.com>
2025-02-03 02:03:05 +0800

    add

---
+++ practical_exercises/10_day_practice/day0/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "we wiling to learn C++" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- practical_exercises/10_day_practice/day1/print.cpp
+++ practical_exercises/10_day_practice/day1/print.cpp
@@ -1,24 +1,25 @@
 /* 打印练习.cpp */
 #include <iostream>
-using namespace std;
+#include <cstdlib>
+
 int main(int argc, char const *argv[]) {
   int i, j, k, f;
   for (i = 1; i <= 4; i++) {
     for (j = 1; j <= 30; j++)
-      cout << " ";
+      std::cout << " ";
     for (k = 1; k <= 8 - 2 * i; k++)
-      cout << " ";
+      std::cout << " ";
     for (f = 1; f <= 2 * i; f++)
-      cout << '*';
-    cout << endl;
+      std::cout << '*';
+    std::cout << std::endl;
   }
   for (i = 1; i <= 3; i++) {
     for (j = 1; j <= 30; j++)
-      cout << " ";
+      std::cout << " ";
     for (f = 1; f <= 7 - 2 * i; f++)
-      cout << '*';
-    cout << endl;
+      std::cout << '*';
+
+    std::cout << std::endl;
   }
-  
   return 0;
 }
