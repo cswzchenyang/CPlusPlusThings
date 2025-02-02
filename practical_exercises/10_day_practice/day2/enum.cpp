63b27c61678561a2fd0f53e37745ba3e1f757e1d
cswzchenyang <2159822551@qq.com>
2025-02-03 02:05:08 +0800

    add

--- practical_exercises/10_day_practice/day2/enum.cpp
+++ practical_exercises/10_day_practice/day2/enum.cpp
@@ -8,9 +8,9 @@ int main(int argc, char const *argv[]) {
   enum weekday wek = s;
   // weekday wek=s;
   for (int i = wek; i != f; i++) {
-    cout << i << endl;
+    cout << i <<" "<< f << endl;
     cout << wek + s << endl;
-    cout << "-------哈哈-------" << endl;
+   // cout << "-------哈哈-------" << endl;
   }
   
   return 0;
--- practical_exercises/10_day_practice/day2/hanoi.cpp
+++ practical_exercises/10_day_practice/day2/hanoi.cpp
@@ -1,25 +1,20 @@
 /* 汉诺塔.cpp */
 #include <iostream>
 using namespace std;
-void move(char A, char B);
-void hanoi(int n, char A, char B, char C);
+void hanoii(int n,char source,char target, char helper);
+
 int main(int argc, char const *argv[]) {
-  cout << "请输入盘子数量：";
-  int disks;
-  cin >> disks;
-  hanoi(disks, 'A', 'B', 'C');
-  
+  hanoii(3, 'q', 'w', 'e');
   return 0;
 }
 
-void move(char A, char B) { cout << A << "->" << B << endl; }
 
-void hanoi(int n, char A, char B, char C) {
-  if (n == 1) {
-    move(A, C);
-  } else {
-    hanoi(n - 1, A, C, B);
-    move(A, C);
-    hanoi(n - 1, B, A, C);
+void hanoii(int n,char source,char target, char helper){
+  if(n==1){
+    cout<<"Move disk 1 from source "<<source<<" to target "<<target<<endl;
+    return;
   }
+  hanoii(n-1,source,helper,target);
+  cout<<"Move disk "<<n<<" from source "<<source<<" to target "<<target<<endl;
+  hanoii(n-1,helper,target,source);
 }
